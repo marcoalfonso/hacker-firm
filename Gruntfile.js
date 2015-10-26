9293ef4e664783280891093fd298fabd33f3d7d9
Marco Lavielle <marcoalfonso@gmail.com>
2015-10-26 15:26:34 +1100

    changed package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -242,9 +242,8 @@ module.exports = function (grunt) {
           src: [
             '<%= yeoman.dist %>/public/{,*/}*.js',
             '<%= yeoman.dist %>/public/{,*/}*.css',
-            '<%= yeoman.dist %>/public/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+            '<%= yeoman.dist %>/public/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg,json}',
             '<%= yeoman.dist %>/public/assets/fonts/*',
-            '<%= yeoman.dist %>/public/assets/particles.json',
             '!<%= yeoman.dist %>/public/bower_components/particles.js',
             '<%= yeoman.dist %>/public/bower_components/particles.js/{,*/}*.js',
           ]
